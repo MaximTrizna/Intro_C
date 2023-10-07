2a1574a7b57b23cf2047d015143e84f6c153a0b0
Maxim <triznamaxim@gmail.com>
2023-10-07 13:43:30 +0700

    Sem3_DZ

--- Examples103_Seminar03/Program.cs
+++ Examples103_Seminar03/Program.cs
@@ -115,9 +115,60 @@ Validate (number); */
 // A (3,6,8); B (2,1,-7) -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-// Решение:
+// Решение 1:
 
+/* Console.Write("Введите число: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+double d = Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)+ Math.Pow(z2 - z1, 2)), 2);
+Console.WriteLine(d); */
 
+// Решение 2:
+/* 
+public class Answer
+{
+    private static double Length(int[] pointA, int[] pointB)
+    {
+      // Введите свое решение ниже
+        double d = Math.Round(Math.Sqrt(Math.Pow(pointB[0] - pointA[0], 2) + Math.Pow(pointB[1] - pointA[1], 2)+ Math.Pow(pointB[2] - pointA[2], 2)), 2);
+        return d;
+    }
+
+  // Не удаляйте и не меняйте метод Main! 
+    public static void Main(string[] args) {
+        int x1, x2, x3, y1, y2, y3;
+
+        if (args.Length >= 6) {
+            x1 = int.Parse(args[0]);
+            x2 = int.Parse(args[1]);
+            x3 = int.Parse(args[2]);
+            y1 = int.Parse(args[3]);
+            y2 = int.Parse(args[4]);
+            y3 = int.Parse(args[5]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            x1 = 3;
+            x2 = 6;
+            x3 = 8;
+            y1 = 2;
+            y2 = 1;
+            y3 = -7;
+        }
+
+        // Не удаляйте строки ниже
+        double result = Length(new int[]{x1, x2, x3}, new int[]{y1, y2, y3});
+        Console.WriteLine($"{result:F2}");
+    }
+} */
 
 //___________________________________________________________________________________________________________
 
@@ -142,6 +193,29 @@ Validate (number); */
 
 // Решение:
 
+/* public class Answer
+{
+   static void ShowCube(int N)
+    {
+      // Введите свое решение ниже
+      for (int i = 1; i <= N; i++)
+        Console.WriteLine(Math.Pow(i, 3));
+    }
 
+  // Не удаляйте и не меняйте метод Main! 
+      static public void Main(string[] args) {
+        int N;
 
+        if (args.Length >= 1) {
+            N = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            N = 6;
+        }
+
+        // Не удаляйте строки ниже
+        ShowCube(N);
+    }
+}
+ */
 //___________________________________________________________________________________________________________
