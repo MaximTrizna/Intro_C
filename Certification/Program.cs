2f6f70d8ab2a0c396678e77d8eea9640b34dca81
Maxim <triznamaxim@gmail.com>
2023-10-25 20:13:33 +0700

    Cert

--- Certification/Program.cs
+++ Certification/Program.cs
@@ -44,3 +44,16 @@ Console.WriteLine(rec(m, n));
 // Решение:
 
 // _____________________________________________________________________________________________________________________________
+
+int rec(int m, int n)
+{
+    if (n == m)
+        return m;
+    return rec(m, n - 1) + n;
+}
+
+Console.Write("Введите элемент: "); 
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите элемент: "); 
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(rec(m, n));
